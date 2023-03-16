336b7a24dc2eebc172532a55172dbc8dc416a8e0
Roger Magnusson <roger.magnusson@gmail.com>
2023-03-16 14:29:11 +0100

    Updated description

--- Fuses/Number Formatter.lua
+++ Fuses/Number Formatter.lua
@@ -1,4 +1,39 @@
-﻿FuRegisterClass("NumberFormatter", CT_Modifier,
+﻿--[[
+
+MIT License
+
+Copyright (c) 2023 Roger Magnusson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+-----------------------------------------------------------------------------
+
+
+	This is a Modifier Fuse Plugin for DaVinci Resove/Fusion that can format a number value in various ways.
+	Right-click a Text field to apply the modifier.
+
+	roger.magnusson@gmail.com
+
+
+]]
+
+FuRegisterClass("NumberFormatter", CT_Modifier,
 {
 	REGS_Name = "Number Formatter",
 	REGS_OpIconString = "NumberFormatter",
